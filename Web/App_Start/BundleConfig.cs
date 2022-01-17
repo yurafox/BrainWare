8d42df7d11e6f5d985c0c98a6f6298aa6a450ad1
Yurii Ishchenko <y_p_i@yahoo.com>
2022-01-17 08:12:07 -0800

    showOrdersList() -> move from Index.cshtml  to js and bundled

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -22,6 +22,10 @@ namespace Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                        "~/Scripts/app/app.js"));
+
         }
     }
 }
---
+++ Web/Scripts/app/app.js
@@ -0,0 +1,30 @@
+﻿function showOrdersList() {
+    var $orders = $('#orders');
+    $.ajax({
+        'url': '/api/order/1',
+        'type': 'GET',
+        'success': function (data) {
+
+            var $orderList = $('<ul/>');
+
+            if (data) {
+                $.each(data,
+                    function (i) {
+                        var $li = $('<li/>').text(this.Description + ' (Total: $' + this.OrderTotal + ')')
+                            .appendTo($orderList);
+
+                        var $productList = $('<ul/>');
+
+                        $.each(this.OrderProducts, function (j) {
+                            var $li2 = $('<li/>').text(this.Product.Name + ' (' + this.Quantity + ' @@ $' + this.Price + '/ea)')
+                                .appendTo($productList);
+                        });
+
+                        $productList.appendTo($li);
+                    });
+
+                $orders.append($orderList);
+            }
+        }
+    });
+}
